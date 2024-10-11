1019a8e33016bb168f25ee7079fa00a41dd12ec9
Matthias Stähli <matthiasstahli@MattBook.local>
2024-10-11 12:30:50 +0200

    initializing task feature

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,15 @@
 import type { StorybookConfig } from "@storybook/angular";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  //stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"], // removes storybook intro stories/components.
+  stories: ["../src/app/components_matthias/**/*.stories.ts"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/angular",
